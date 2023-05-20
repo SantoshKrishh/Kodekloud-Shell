1e4fd978fd70201470de940f98f8d645e7f2e37d
Santosh <santoshkumar.kollati@gmail.com>
2023-05-20 12:44:09 -0600

    basic contional logic example

---
+++ conditional_logic_ex.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+mission_name=$1
+
+mkdir $mission_name
+
+rocket-add $mission_name
+
+rocket-start-power $mission_name
+rocket-internal-power $mission_name
+rocket-start-sequence $mission_name
+rocket-start-engine $mission_name
+rocket-lift-off $mission_name
+
+rocket_status=$(rocket-status $mission_name)
+#Basic conditional logic example
+echo "The status of launch is $rocket_status"
+
+if [ $rocket_status = "failed" ]
+then
+  rocket-debug $mission_name
+fi
