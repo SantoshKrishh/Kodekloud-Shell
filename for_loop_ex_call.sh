570f3d646b1ae5e8c910b960091d963892425faf
Santosh <santoshkumar.kollati@gmail.com>
2023-05-31 05:32:46 -0600

    for lopp ex to call another script

---
+++ for_loop_ex_call.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Develop a new script at /home/bob/launch-rockets.sh to call the create-and-launch-rocket script to launch 5 rockets 
+# for the following missions using a for loop.
+# lunar-mission, mars-mission, jupiter-mission, saturn-mission, mercury-mission
+
+for mission in lunar-mission mars-mission jupiter-mission saturn-mission mercury-mission
+do
+        bash /home/bob/create-and-launch-rocket $mission
+done
